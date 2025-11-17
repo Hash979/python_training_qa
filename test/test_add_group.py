9a3ccce7ddfb06bd9c276d1d5556fedab143b06c
hshus <hshus087@gmail.com>
2025-11-17 18:18:12 +0300

    Выделен класс SessionHelper

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -11,14 +11,14 @@ def app(request):
     return  fixture
 
 def test_add_group(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_new_group(Group(name="sss", header="sss", footer="sss"))
-    app.logout()
+    app.session.logout()
 
 def test_add_empty_group(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_new_group(Group(name="", header="", footer=""))
-    app.logout()
+    app.session.logout()
 
 
 
