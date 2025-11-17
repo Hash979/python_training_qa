9cb3c2592760d5b5fb76276f9050c989aaaccfd3
hshus <hshus087@gmail.com>
2025-11-17 18:59:03 +0300

    Общая фикстура + выделен класс для Контактов

---
+++ fixture/contact.py
@@ -0,0 +1,41 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+class ContactHelper:
+
+    def __init__(self, app):
+        self.app = app
+
+    def create_new_contact(self, contact):
+        wd = self.app.wd
+        self.open_add_new_contact_page()
+        wd.find_element(By.NAME, "firstname").send_keys(contact.firstname)
+        wd.find_element(By.NAME, "middlename").send_keys(contact.middlename)
+        wd.find_element(By.NAME, "lastname").send_keys(contact.lastname)
+        wd.find_element(By.NAME, "nickname").send_keys(contact.nickname)
+        wd.find_element(By.NAME, "photo").send_keys(contact.photo_path)
+        wd.find_element(By.NAME, "company").send_keys(contact.company)
+        wd.find_element(By.NAME, "title").send_keys(contact.title)
+        wd.find_element(By.NAME, "address").send_keys(contact.address)
+        wd.find_element(By.NAME, "home").send_keys(contact.home)
+        wd.find_element(By.NAME, "mobile").send_keys(contact.mobile)
+        wd.find_element(By.NAME, "work").send_keys(contact.work)
+        wd.find_element(By.NAME, "fax").send_keys(contact.fax)
+        wd.find_element(By.NAME, "email").send_keys(contact.email)
+        wd.find_element(By.NAME, "email2").send_keys(contact.email2)
+        wd.find_element(By.NAME, "email3").send_keys(contact.email3)
+        wd.find_element(By.NAME, "homepage").send_keys(contact.homepage)
+
+        # date
+        Select(wd.find_element(By.NAME, "bday")).select_by_visible_text(contact.bday)
+        Select(wd.find_element(By.NAME, "bmonth")).select_by_visible_text(contact.bmonth)
+        wd.find_element(By.NAME, "byear").send_keys(contact.byear)
+        Select(wd.find_element(By.NAME, "aday")).select_by_visible_text(contact.aday)
+        Select(wd.find_element(By.NAME, "amonth")).select_by_visible_text(contact.amonth)
+        wd.find_element(By.NAME, "ayear").send_keys(contact.ayear)
+
+        # Sace contact
+        wd.find_element(By.XPATH, "//div[@id='content']/form/input[20]").click()
+
+    def open_add_new_contact_page(self):
+        wd = self.app.wd
+        wd.find_element(By.LINK_TEXT, "add new").click()
