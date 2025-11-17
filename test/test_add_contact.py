babca9ee1575160d1f1c5e6e1141cc8368cc8ecc
hshus <hshus087@gmail.com>
2025-11-17 18:07:06 +0300

    Созданы пакеты

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException, NoAlertPresentException
-from Contact import Contact
+from model.Contact import Contact
 
 
 class TestAddContact(unittest.TestCase):
