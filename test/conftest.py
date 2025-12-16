821dc880651d3fd6b297c3758629762736a78db1
hshus <hshus087@gmail.com>
2025-12-17 00:45:03 +0300

    Реализованы опции командной строки, позволяющие указывать тип браузера и адрес

--- test/conftest.py
+++ test/conftest.py
@@ -5,12 +5,14 @@ fixture = None
 @pytest.fixture
 def app(request):
     global fixture
+    browser = request.config.getoption("--browser")
+    base_url = request.config.getoption("--baseurl")
     if fixture is None:
-        fixture = Application()
+        fixture = Application(browser=browser, base_url=base_url)
         fixture.session.login(username="admin", password="secret")
     else:
         if not fixture.is_valid():
-            fixture = Application()
+            fixture = Application(browser=browser, base_url=base_url)
             fixture.session.login(username="admin", password="secret")
     fixture.session.ensure_login(username="admin", password="secret")
     return fixture
@@ -22,3 +24,7 @@ def stop(request):
         fixture.destroy()
     request.addfinalizer(fin)
     return  fixture
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="firefox")
+    parser.addoption("--baseurl", action="store", default="http://localhost/addressbook/")
