81b0a6cf1cf3e2e14512ae64bbf78bff0087b375
hshus <hshus087@gmail.com>
2025-12-17 00:19:50 +0300

    Добавлена случайность в создания контактов и групп

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,32 +1,77 @@
 # -*- coding: utf-8 -*-
 from model.Contact import Contact
+import pytest
+import random
+import string
 
-def test_add_contact(app):
-    old_contacts = app.contact.get_contact_list()
-    contacts = Contact(
-        firstname="Anton",
-        middlename="Velikoborets",
-        lastname="Anton",
-        nickname="Hash",
-        photo_path=u"C:\\test.png",
-        company="SecCode",
-        title="sss",
-        address="Groove street",
-        home="8992",
-        mobile="+79211908672",
-        work="1232",
-        fax="1233",
-        email="hshus087@gmail.com",
-        email2="hshus087@gmail.com",
-        email3="hshus087@gmail.com",
-        homepage="hshus087@gmail.com",
-        bday="22",
-        bmonth="November",
-        byear="2001",
-        aday="22",
-        amonth="December",
-        ayear="2002"
+def random_day():
+    return str(random.randint(1, 28))
+
+
+def random_year(start=1970, end=2025):
+    return str(random.randint(start, end))
+
+
+def random_month():
+    months = [
+        "January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+    ]
+    return random.choice(months)
+
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " "*5
+    return prefix + "".join(random.choice(symbols) for i in range(random.randrange(maxlen)))
+
+
+def random_phone(maxlen=10):
+    return "".join(random.choice(string.digits) for i in range(random.randrange(5, maxlen)))
+
+
+def random_email(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits
+    return prefix + "".join(random.choice(symbols) for i in range(random.randrange(maxlen))) + "@mail.com"
+
+testdata = [
+    Contact(firstname="", lastname=""),
+    Contact(firstname=random_string("fn_", 10), lastname=""),
+    Contact(firstname="", lastname=random_string("ln_", 10)),
+    Contact(
+        firstname=random_string("fn_", 10),
+        lastname=random_string("ln_", 10),
+        email=random_email("email_", 10)
+    ),
+    Contact(
+        firstname=random_string("fn_", 10),
+        middlename=random_string("mn_", 10),
+        lastname=random_string("ln_", 10),
+        nickname=random_string("nick_", 10),
+        company=random_string("comp_", 10),
+        title=random_string("title_", 10),
+        address=random_string("addr_", 20),
+        home=random_phone(),
+        mobile=random_phone(),
+        work=random_phone(),
+        fax=random_phone(),
+        email=random_email("email_", 10),
+        email2=random_email("email2_", 10),
+        email3=random_email("email3_", 10),
+        homepage=random_string("site_", 10),
+        bday=random_day(),
+        bmonth=random_month(),
+        byear=random_year(),
+        aday=random_day(),
+        amonth=random_month(),
+        ayear=random_year()
     )
+]
+
+
+
+@pytest.mark.parametrize("contacts", testdata, ids=[repr(x) for x in testdata])
+
+def test_add_contact(app, contacts):
+    old_contacts = app.contact.get_contact_list()
     app.contact.create_contact(contacts)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
