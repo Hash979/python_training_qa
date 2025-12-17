55f64f708b16de21a42659759fe348e9776b3271
hshus <hshus087@gmail.com>
2025-12-18 02:10:50 +0300

    Добавлен тест на проверку информации о контактах в БД и на главной странице

--- test/test_db_matches_ui.py
+++ test/test_db_matches_ui.py
@@ -1,4 +1,6 @@
 from model.group import Group
+from model.Contact import Contact
+import re
 
 def test_group_list(app, db):
     ui_list = app.group.get_group_list()
@@ -6,3 +8,26 @@ def test_group_list(app, db):
         return Group(id=group.id, name=group.name.strip())
     db_list = map(clean, db.get_group_list())
     assert sorted(ui_list, key=Group.id_or_max) == sorted(db_list, key=Group.id_or_max)
+
+def test_contact_list(app, orm):
+    ui_list = app.contact.get_contact_list()
+    def clean(contact):
+        def clear(s):
+            return re.sub("[() -]", "", s) if s else ""
+
+        def merge_phones(contact):
+            return "\n".join(filter(None, map(clear, [contact.home, contact.mobile, contact.work])))
+
+        def merge_emails(contact):
+            return "\n".join(filter(None, map(lambda x: x.replace(" ", "") if x else "",
+                                              [contact.email, contact.email2, contact.email3])))
+        return Contact(
+            id=contact.id,
+            firstname=contact.firstname.strip() if contact.firstname else "",
+            lastname=contact.lastname.strip() if contact.lastname else "",
+            address=contact.address.strip() if contact.address else "",
+            all_phones=merge_phones(contact),
+            all_emails=merge_emails(contact)
+        )
+    orm_list = map(clean, orm.get_contact_list())
+    assert sorted(ui_list, key=Contact.id_or_max) == sorted(orm_list, key=Contact.id_or_max)
