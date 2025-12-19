e431519e206b82218a16779067d79b4b66b0f608
hshus <hshus087@gmail.com>
2025-12-19 21:39:58 +0300

    Fix test

--- test/test_add_contact_into_group.py
+++ test/test_add_contact_into_group.py
@@ -12,8 +12,6 @@ def test_delete_some_contact(app, orm):
     contact = random.choice(old_contacts)
     old_groups = orm.get_group_list()
     group = random.choice(old_groups)
-    app.contact.add_contact_into_group(contact.id, group.id)
-    assert any(c.id == contact.id for c in orm.get_contacts_in_group(group))
     app.contact.remove_contact_from_group(contact.id, group.id)
     assert all(c.id != contact.id for c in orm.get_contacts_in_group(group))
 
