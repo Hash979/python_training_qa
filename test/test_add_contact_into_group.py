6bc15864ad7f7b3189da130b78cdb86fc2b1f254
hshus <hshus087@gmail.com>
2025-12-21 12:24:21 +0300

    Исправлен тесты на добавление/удаление контакта из группы2

--- test/test_add_contact_into_group.py
+++ test/test_add_contact_into_group.py
@@ -25,19 +25,6 @@ def test_delete_some_contact(app, orm):
     app.contact.remove_contact_from_group(contact.id, group.id)
     assert all(c.id != contact.id for c in orm.get_contacts_in_group(group))
 
-
-def test_add_contact(app, orm):
-    if len(orm.get_contact_list()) == 0:
-        app.contact.create_contact(Contact(firstname="test"))
-    if len(orm.get_group_list()) == 0:
-        app.group.Create(Group(name="test"))
-    old_contacts = orm.get_contact_list()
-    contact = random.choice(old_contacts)
-    old_groups = orm.get_group_list()
-    group = random.choice(old_groups)
-    app.contact.add_contact_into_group(contact.id, group.id)
-    assert any(c.id == contact.id for c in orm.get_contacts_in_group(group))
-
 def test_add_contact(app, orm):
     if len(orm.get_contact_list()) == 0:
         app.contact.create_contact(Contact(firstname="test"))
