32664da56b5a2167360161a5e8b984cdc8ad228f
hshus <hshus087@gmail.com>
2025-12-21 21:24:46 +0300

    open home page fix

--- fixture/application.py
+++ fixture/application.py
@@ -22,7 +22,7 @@ class Application:
 
     def open_home_page(self):
         wd = self.wd
-        if not (wd.current_url.endswith("/addressbook/") and len(wd.find_elements(By.value, "Send e-Mail")) > 0):
+        if not (wd.current_url.endswith("/addressbook/") and len(wd.find_elements(By.NAME, "Send e-Mail")) > 0):
             wd.get(self.base_url)
 
     def destroy(self):
